1b3a86d9440eab6fd7e6e0979cd0855e5f20fe64
Martin Leo <martin.christoffer.leo@gmail.com>
2026-05-26 08:56:38 +0200

    changed to Path.Join

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@
                 try
                 {
                     Console.WriteLine("Försöker läsa fil och räkna...");
-                    var path = Path.Combine(AppContext.BaseDirectory, "numbers.txt");
+                    var path = Path.Join(AppContext.BaseDirectory, "numbers.txt");
                     var result = ProcessFile(path);
                   
                     Console.WriteLine($"\nResultat: {result}");
